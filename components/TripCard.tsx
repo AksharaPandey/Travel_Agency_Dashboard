db365592819c0d09edc297cce79c8823aef1abe1
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2025-04-29 14:11:51 +0530

    Tripcard Details

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,9 +1,11 @@
+import { Link, useLocation } from "react-router"
 
 const TripCard = ({id, name, location, imageUrl, tags, price}:TripCardProps) => {
+  const path=useLocation();
   return (
-    <div>
-      TripCard
-    </div>
+    <Link to={path.pathname==='/' || path.pathname.startsWith('/travel') ? '/travel/${id}': '/trips/${id}'}>
+       <img src={imageUrl} alt={name}/>
+    </Link>
   )
 }
 
