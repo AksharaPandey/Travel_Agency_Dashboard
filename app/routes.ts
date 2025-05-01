d7e2466466964cc7588179e147d99c4a5f48c445
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2025-05-01 15:06:52 +0530

    Sign-in

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
 import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 
 export default [
+    route('sign-in', './routes/root/sign-in.tsx'),  // Path to sign-in as a string
     layout('routes/admin/admin-layout.tsx', [  // Path to layout as a string
         route('dashboard', './routes/admin/dashboard.tsx'),  // Path to dashboard as a string
         route('all-users', './routes/admin/all-users.tsx'),
